d07692e6a528f6c40654ea587a16cd0b0364e1f4
Bhawuk Arora <bhawukarora042@gmail.com>
2026-01-30 03:22:22 +0530

    test memory fill isssue in deploy

--- backend/app/services/embeddings.py
+++ backend/app/services/embeddings.py
@@ -1,6 +1,15 @@
+# from sentence_transformers import SentenceTransformer
+
+# model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# def embed_texts(texts):
+#     return model.encode(texts, show_progress_bar=True)
+
+
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+# Change from all-MiniLM-L6-v2 to a lighter version
+model = SentenceTransformer("paraphrase-MiniLM-L3-v2") 
 
 def embed_texts(texts):
     return model.encode(texts, show_progress_bar=True)
